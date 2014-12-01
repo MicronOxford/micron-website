f80ff5c683959bf6db6261798fce966a5ae1245a
alanwainman <alanwainman@gmail.com>
2014-12-01 15:14:02 +0000

    ONBI course notes added

---
+++ ONBI_Course.php
@@ -0,0 +1,107 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+
+<head>
+  <title>Micron Oxford Advanced Bioimaging: ONBI Lecture Course: Week 1</title>
+  <meta name=description content="Micron Oxford FAQs, protocols, lectures and other resources">
+  <meta name=keywords content="Micron Oxford microscopy resources FAQs protocols Wiki lectures bioimaging links">
+  <meta http-equiv="Content-Type" CONTENT="text/html; charset=ISO-8859-1">
+  <link rel="stylesheet" type="text/css" href="styles/micron_main.css" >
+  <link rel="stylesheet" type="text/css" href="styles/micron_resources.css" >
+  <link rel="icon"
+      type="image/ico"
+      href="/images/MicronMu.ico" >
+      <?php include 'analysistracking.html'; ?>
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+
+<!-- Body has container_all which is divided into header, navigation, mainbody, footer -->
+<body id="resources" onLoad="MM_preloadImages('images/2_stomata.jpg','images/lecture_01a.jpg','images/lecture_00a.jpg','images/lecture_02a.jpg')">
+
+  <?php include 'scripts/checkViewerIP.php'; ?>
+
+  <div id="container_all">
+
+    <!-- include html file containing header and navigation divs common to all pages -->
+    <?php include 'micron_headnav.html'; ?>
+
+    <!-- navigation submenu for this page -->
+
+    <div id="mainbody">
+    <!-- mainbody is subdivided into leftpanel and smaller rightpanel -->
+    <h3>ONBI Course 2014-2015: Week 1 Principles of light microscope design and fluorescence imaging	</h3> Click on image for lecture notes
+    <div >
+    
+    <a href="resources/ONBI_Lecture_01_timetable.pdf"> <img id="slides" src="images/ONBI_lecture_01.png"  width="170" height="131" alt="ONBI Lecture 1"></a>
+    
+    <a href="resources/ONBI_Lecture_02_introduction.pdf"><img src="images/ONBI_lecture_02.png" alt="ONBI Lecture 2" width="170" height="131" id="slides"></a>
+    
+    <a href="resources/ONBI_Lecture_03_imageformation_resolution.pdf"><img id="slides" src="images/ONBI_lecture_03.png"  width="170" height="131" alt="ONBI Lecture 3"></a>
+    
+    <a href="resources/ONBI_Lecture_0405_contrast_enhancement_fluorescence.pdf"><img id="slides" src="images/ONBI_lecture_04.png"  width="170" height="130" alt="ONBI Lecture 4 and 5"></a>
+    
+    <a href="resources/ONBI_Lecture_06_fluorescentdyes.pdf"><img src="images/ONBI_lecture_06.png" alt="ONBI Lecture 6" width="170" height="131" id="slides"></a> 
+    
+    <a href="resources/ONBI_Lecture_07_detectors.pdf"><img src="images/ONBI_lecture_07.png" alt="ONBI Lecture 7" width="170" height="131" id="slides"></a>
+    
+    <a href="resources/ONBI_Lecture_08_live_cell_imaging.pdf"><img id="slides" src="images/ONBI_lecture_08.png"  width="170" height="131" alt="ONBI Lecture 8"></a>
+    
+    <a href="resources/ONBI_Lecture_09_confocals.pdf"><img  src="images/ONBI_lecture_09.png"  width="170" height="130" alt="ONBI Lecture 9" id="slides"></a>
+    
+    <a href="resources/ONBI_Lecture_10_lightsheet.pdf"><img id="slides" src="images/ONBI_lecture_10.png"  width="170" height="130" alt="ONBI Lecture 10"></a>
+    
+    <a href="resources/ONBI_Lecture_11_widefieldbespoke.pdf"><img id="slides" src="images/ONBI_lecture_11.png"  width="170" height="130" alt="ONBI Lecture 11"></a>
+    
+    <a href="resources/ONBI_Lecture_12_f_techniques.pdf"><img id="slides" src="images/ONBI_lecture_12.png"  width="170" height="130" alt="ONBI Lecture 12"></a>
+    
+    <a href="resources/ONBI_Lecture_13_STED_STED-FCS.pdf"><img id="slides" src="images/ONBI_lecture_13.png"  width="170" height="130" alt="ONBI Lecture 13"></a>
+    
+    <a href="resources/ONBI_Lecture_14_localisation_microscopy.pdf"><img id="slides" src="images/ONBI_lecture_14.png"  width="170" height="130" alt="ONBI Lecture 14"></a>
+    
+    <a href="resources/ONBI_Lecture_15_3D-SIM.pdf"><img id="slides" src="images/ONBI_lecture_15.png"  width="170" height="130" alt="ONBI Lecture 15"></a>
+    
+    <a href="resources/ONBI_Lecture_16_EM.pdf"><img id="slides" src="images/ONBI_lecture_16.png"  width="170" height="130" alt="ONBI Lecture 16"></a>
+    
+    
+    
+    </div>
+
+      
+
+     
+
+    </div>
+
+
+    <!-- include html file containing footer div common to all pages -->
+    <?php include 'micron_footer.html' ?>
+
+  </div>
+</body>
+
+
+</html>
