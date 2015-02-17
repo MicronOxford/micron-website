e6352fbdf3d6fc6139c686e34a94cfee90615c60
alanwainman <alanwainman@gmail.com>
2015-02-17 16:21:07 +0000

    updates to micron publications page

--- research/publications.php
+++ research/publications.php
@@ -16,9 +16,315 @@
     <div id="mainbody">
 
     <h3>Micron Publications</h3>
+    <h4>2015</h4>
+    <ul class="publicationlist">
+    <li>
+    Diepold A, Kudryashev M, Delalez NJ, Berry RM, Armitage JP.
+    Composition, Formation, and Regulation of the Cytosolic C-ring, a Dynamic Component of the Type III Secretion Injectisome.
+    PLoS Biol. 2015 Jan 15;13(1):e1002039. 
+    </li>
+    </ul>
+    <h4>2014</h4>
+    <ul class="publicationlist">
+    <li>
+    Lowe N, Rees JS, Roote J, Ryder E, Armean IM, Johnson G, Drummond E, Spriggs H, Drummond J, Magbanua JP, Naylor H, Sanson B, Bastock R, Huelsmann S, Trovisco V, Landgraf M, Knowles-Barley S, Armstrong JD, White-Cooper H, Hansen C, Phillips RG; UK Drosophila Protein Trap Screening Consortium, Lilley KS, Russell S, St Johnston D.
+    Analysis of the expression patterns, subcellular
+localisations and interaction partners of Drosophila proteins using a pigP protein trap library. 
+Development 141: 3994-4005. 
+	</li>
+	<li>
+	Halstead, J. M., Lin, Y.Q., Durraine, L., Hamilton R.S., Ball, 	G., Neely, G.G., Bellen, H.J. and Davis, I. 
+    Syncrip/hnRNPQ influences synaptic
+transmission and regulates BMP retrograde signaling across the Drosophila
+neuromuscular synapse. 
+Biology Open, 3: 839-49. 
+	</li>
+    <li>McDermott, S., Yang, L., Halstead, J., Russell S. Hamilton, R., Meignin, C. and Davis, I.
+    Drosophila Syncrip modulates the expression of mRNAs
+encoding key synaptic proteins required for morphology at the neuromuscular
+junction.
+	RNA, 20: 1593-1606. 
+    </li>
+    <li>
+    Parton, R.M., Davidson, A., Davis, I., Weil T.T. 
+    Subcellular mRNA localisation at a glance. 
+	J Cell Sci. 15: 2127-33.
+    </li>
+    <li>
+    Bailey MW, Bisicchia P, Warren BT, Sherratt DJ, Männik J.
+    Evidence for Divisome Localization Mechanisms Independent of the Min System and SlmA in Escherichia coli.
+    PLoS Genet. 2014 Aug 7;10(8):e1004504. 
+    </li>
+    <li>
+    Nicolas E, Upton AL, Uphoff S, Henry O, Badrinarayanan A, Sherratt D.
+    The SMC complex MukBEF recruits topoisomerase IV to the origin of replication region in live Escherichia coli.
+    MBio. 2014 Feb 11;5(1):e01001-13. 
+    </li>
+    <li>
+    Uphoff S, Sherratt DJ, Kapanidis AN.
+    Visualizing protein-DNA interactions in live bacterial cells using photoactivated single-molecule tracking.
+    J Vis Exp. 2014 Mar 10;(85). 
+    </li>
+    <li>
+    Lesterlin C, Ball G, Schermelleh L, Sherratt DJ.
+    RecA bundles mediate homology pairing between distant sisters during DNA break repair.
+    Nature. 2014 Feb 13;506(7487):249-53. 
+    </li>
+    <li>
+    Coker H, Brockdorff N.
+    SMCHD1 accumulates at DNA damage sites and facilitates the repair of DNA double-strand breaks.
+    J Cell Sci. 2014 May 1;127(Pt 9):1869-74. 
+    </li>
+    <li>
+    Cerase A, Smeets D, Tang YA, Gdula M, Kraus F, Spivakov M, Moindrot B, Leleu M, Tattermusch A, Demmerle J, Nesterova TB, Green C, Otte AP, Schermelleh L, Brockdorff N.
+    Spatial separation of Xist RNA and polycomb proteins revealed by superresolution microscopy.
+    Proc Natl Acad Sci U S A. 2014 Feb 11;111(6):2235-40. 
+    </li>
+    <li>
+    Cooper S, Dienstbier M, Hassan R, Schermelleh L, Sharif J, Blackledge NP, De Marco V, Elderkin S, Koseki H, Klose R, Heger A, Brockdorff N.
+    Targeting polycomb to pericentric heterochromatin in embryonic stem cells reveals a role for H2AK119u1 in PRC2 recruitment.
+    Cell Rep. 2014 Jun 12;7(5):1456-70. 
+    </li>
+    <li>
+    Smeets D, Markaki Y, Schmid VJ, Kraus F, Tattermusch A, Cerase A, Sterr M, Fiedler S, Demmerle J, Popken J, Leonhardt H, Brockdorff N, Cremer T, Schermelleh L, Cremer M. 
+	Three-dimensional super-resolution microscopy of the inactive X
+chromosome territory reveals a collapse of its active nuclear compartment harboring distinct Xist RNA foci.
+	Epigenetics Chromatin. 2014 Apr 28;7:8. 
+    </li>
+    <li>Fairhead M, Krndija D, Lowe ED, Howarth M.
+    Plug-and-Play Pairing via Defined Divalent Streptavidins.
+    J Mol Biol. 2014 Jan 9;426(1):199-214. 
+    </li>
+    <li>
+    Fierer JO, Veggiani G, Howarth M.
+    SpyLigase peptide-peptide ligation polymerizes affibodies to enhance magnetic cancer cell capture.
+    Proc Natl Acad Sci U S A. 2014 Apr 1;111(13):E1176-81. 
+    </li>
+    <li>
+    Espert A, Uluocak P, Bastos RN, Mangat D, Graab P, Gruneberg U.
+    PP2A-B56 opposes Mps1 phosphorylation of Knl1 and thereby promotes spindle assembly checkpoint silencing.
+    J Cell Biol. 2014 Sep 29;206(7):833-42. 
+    </li>
+    <li>
+    Conduit PT, Richens JH, Wainman A, Holder J, Vicente CC, Pratt MB, Dix CI, Novak ZA, Dobbie IM, Schermelleh L, Raff JW.
+    A molecular mechanism of mitotic centrosome assembly in Drosophila.
+    Elife. 2014 Aug 22:e03399. 
+    </li>
+    <li>
+    Novak ZA, Conduit PT, Wainman A, Raff JW.
+    Asterless licenses daughter centrioles to duplicate for the first time in Drosophila embryos.
+    Curr Biol. 2014 Jun 2;24(11):1276-82. 
+    </li>
+    <li>
+    Conduit PT, Feng Z, Richens JH, Baumbach J, Wainman A, Bakshi SD, Dobbelaere J, Johnson S, Lea SM, Raff JW.
+    The centrosome-specific phosphorylation of Cnn by Polo/Plk1 drives Cnn scaffold assembly and centrosome maturation.
+    Dev Cell. 2014 Mar 31;28(6):659-69. 
+    </li>
+    <li>
+    Gerondopoulos A, Bastos RN, Yoshimura S, Anderson R, Carpanini S, Aligianis I, Handley MT, Barr FA.
+    Rab18 and a Rab18 GEF complex are required for normal ER structure.
+    J Cell Biol. 2014 Jun 9;205(5):707-20. 
+    </li>
+    <li>
+    Langemeyer L, Nunes Bastos R, Cai Y, Itzen A, Reinisch KM, Barr FA.
+    Diversity and plasticity in Rab GTPase nucleotide release mechanism has consequences for Rab activation and inactivation.
+    Elife. 2014 Feb 11;3:e01623. 
+    </li>
+    <li>
+    Skourti-Stathaki K, Kamieniarz-Gdula K, Proudfoot NJ.
+    R-loops induce repressive chromatin marks over mammalian gene terminators.
+    Nature. 2014 Dec 18;516(7531):436-9. 
+    </li>
+    <li>
+    Kaufmann R, Schellenberger P, Seiradake E, Dobbie IM, Jones EY, Davis I, Hagen C, Grünewald K.
+    Super-resolution microscopy using standard fluorescent proteins in intact cells under cryo-conditions.
+    Nano Lett. 2014 Jul 9;14(7):4171-5. 
+    </li>
+    <li>
+    Schellenberger P, Kaufmann R, Siebert CA, Hagen C, Wodrich H, Grünewald K.
+    High-precision correlative fluorescence and electron cryo microscopy using two independent alignment markers.
+    Ultramicroscopy. 2014 Aug;143:41-51. 
+    </li>
+    <li>
+    Osseforth C, Moffitt JR, Schermelleh L, Michaelis J.
+    Simultaneous dual-color 3D STED microscopy.
+    Opt Express. 2014 Mar 24;22(6):7028-39. 
+    </li>
+    <li>
+    Baboo S, Bhushan B, Jiang H, Grovenor CR, Pierre P, Davis BG, Cook PR.
+    Most human proteins made in both nucleus and cytoplasm turn over within minutes.
+    PLoS One. 2014 Jun 9;9(6):e99346.
+    </li>
+    <li>
+    Barkalina N, Jones C, Kashir J, Coote S, Huang X, Morrison R, Townley H, Coward K.
+    Effects of mesoporous silica nanoparticles upon the function of mammalian sperm in vitro.
+    Nanomedicine. 2014 May;10(4):859-70. 
+    </li>
+    <li>
+    Hsiao LC, Perbellini F, Gomes RS, Tan JJ, Vieira S, Faggian G, Clarke K, Carr CA.
+    Murine cardiosphere-derived cells are impaired by age but not by cardiac dystrophic dysfunction.
+    Stem Cells Dev. 2014 May 1;23(9):1027-36. 
+    </li>
+    <li>
+    Nordlander S, Pott J, Maloy KJ.
+    NLRC4 expression in intestinal epithelial cells mediates protection against an enteric pathogen.
+    Mucosal Immunol. 2014 Jul;7(4):775-85. 
+    </li>
+    <li>
+    Rezával C, Nojima T, Neville MC, Lin AC, Goodwin SF.
+    Sexually dimorphic octopaminergic neurons modulate female postmating behaviors in Drosophila.
+    Curr Biol. 2014 Mar 31;24(7):725-30. 
+    </li>
+    <li>
+    Song-Zhao GX, Srinivasan N, Pott J, Baban D, Frankel G, Maloy KJ.
+    Nlrp3 activation in the intestinal epithelium protects against a mucosal pathogen.
+    Mucosal Immunol. 2014 Jul;7(4):763-74. 
+    </li>
+    <li>
+    White E, Schlackow M, Kamieniarz-Gdula K, Proudfoot NJ, Gullerova M.
+    Human nuclear Dicer restricts the deleterious accumulation of endogenous double-stranded RNA.
+    Nat Struct Mol Biol. 2014 Jun;21(6):552-9. 
+    </li>
+    <li>
+    Yeh YC, Tang MJ, Parekh AB.
+    Caveolin-1 alters the pattern of cytoplasmic Ca2+ oscillations and Ca2+-dependent gene expression by enhancing leukotriene receptor desensitization.
+    J Biol Chem. 2014 Jun 20;289(25):17843-53. 
+    </li>
+    
+	
+	
+    
+    
+
+ 
+
+
+
+
+    </ul>
+    <h4>2013</h4>
+    <ul class="publicationlist">
+    <li>Chiu SW, Roberts MA, Leake MC, Armitage JP.
+    Positioning of chemosensory proteins and FtsZ through the Rhodobacter sphaeroides cell cycle.
+    Mol Microbiol. 2013 Oct;90(2):322-37.
+    </li>
+    <li>Cundell MJ, Bastos RN, Zhang T, Holder J, Gruneberg U, Novak B, Barr FA.
+    The BEG (PP2A-B55/ENSA/Greatwall) pathway ensures cytokinesis follows chromosome separation.
+    Mol Cell. 2013 Nov 7;52(3):393-405. 
+    </li>
+    <li>Nunes Bastos R, Gandhi SR, Baron RD, Gruneberg U, Nigg EA, Barr FA.
+    Aurora B suppresses microtubule dynamics and limits central spindle size by locally activating KIF4A.
+    J Cell Biol. 2013 Aug 19;202(4):605-21. 
+    </li>
+    <li>Schellenberger P, Kaufmann R, Siebert CA, Hagen C, Wodrich H, Grünewald K.
+    High-precision correlative fluorescence and electron cryo microscopy using two independent alignment markers.
+    Ultramicroscopy. 2013 Oct 19. 
+    </li>
+    <li>Seiradake E, Schaupp A, del Toro Ruiz D, Kaufmann R, Mitakidis N, Harlos K, Aricescu AR, Klein R, Jones EY.
+    Structurally encoded intraclass differences in EphA clusters drive distinct cell responses.
+    Nat Struct Mol Biol. 2013 Aug;20(8):958-64. 
+    </li>
+    <li>
+    Oliveira RA, Nasmyth K.
+    Cohesin cleavage is insufficient for centriole disengagement in Drosophila.
+    Curr Biol. 2013 Jul 22;23(14):R601-3. 
+    </li>
+    <li>Chan KL, Gligoris T, Upcher W, Kato Y, Shirahige K, Nasmyth K, Beckouët F.
+    Pds5 promotes and protects cohesin acetylation.
+    Proc Natl Acad Sci U S A. 2013 Aug 6;110(32):13020-5. 
+    </li>
+    <li>
+    Eichinger CS, Kurze A, Oliveira RA, Nasmyth K.
+    Disengaging the Smc3/kleisin interface releases cohesin from Drosophila chromosomes during interphase and mitosis.
+    EMBO J. 2013 Mar 6;32(5):656-65. 
+    </li>
+    <li>
+    Franz A, Roque H, Saurya S, Dobbelaere J, Raff JW.
+    CP110 exhibits novel regulatory activities during centriole assembly in Drosophila.
+    J Cell Biol. 2013 Dec 9;203(5):785-99. 
+    </li>
+    <li>
+    Lesterlin C, Ball G, Schermelleh L, Sherratt DJ.
+    RecA bundles mediate homology pairing between distant sisters during DNA break repair.
+    Nature. 2013 Dec 22. 
+    </li>
+    <li>
+    Markaki Y, Smeets D, Cremer M, Schermelleh L.
+    Fluorescence in situ hybridization applications for super-resolution 3D structured illumination microscopy.
+    Methods Mol Biol. 2013;950:43-64. 
+    </li>
+    <li>
+    Reyes-Lamothe R, Tran T, Meas D, Lee L, Li AM, Sherratt DJ, Tolmasky ME.
+    High-copy bacterial plasmids diffuse in the nucleoid-free space, replicate stochastically and are randomly partitioned at cell division.
+    Nucleic Acids Res. 2013 Oct 16. 
+    </li>
+    <li>
+    Cosgrove C, Ussher JE, Rauch A, Gärtner K, Kurioka A, Hühn MH, Adelmann K, Kang YH, Fergusson JR, Simmonds P, Goulder P, Hansen TH, Fox J, Günthard HF, Khanna N, Powrie F, Steel A, Gazzard B, Phillips RE, Frater J, Uhlig H, Klenerman P.
+    Early and nonreversible decrease of CD161++ /MAIT cells in HIV infection.
+    Blood. 2013 Feb 7;121(6):951-61. 
+    </li>
+    <li>
+    Hughes J, Frago S, Bühnemann C, Carter EJ, Hassan AB.
+    Maternal transmission of a humanised Igf2r allele results in an Igf2 dependent hypomorphic and non-viable growth phenotype.
+    PLoS One. 2013;8(2):e57270. 
+    </li>
+    <li>
+    Zhang YX, van Oosterwijk JG, Sicinska E, Moss S, Remillard SP, van Wezel T, Bühnemann C, Hassan AB, Demetri GD, Bovée JV, Wagner AJ.
+    Functional profiling of receptor tyrosine kinases and downstream signaling in human chondrosarcomas identifies pathways for rational targeted therapy.
+    Clin Cancer Res. 2013 Jul 15;19(14):3796-807. 
+    </li>
+    <li>
+    Duncan CJ, Williams JP, Schiffner T, Gärtner K, Ochsenbauer C, Kappes J, Russell RA, Frater J, Sattentau QJ.
+    High multiplicity HIV-1 infection and neutralizing antibody evasion mediated by the macrophage-T cell virological synapse.
+    J Virol. 2014 Feb;88(4):2025-34. 
+    </li>
+    <li>
+    Barkalina N1, Jones C1, Kashir J1, Coote S1, Huang X2, Morrison R2, Townley H2, Coward K3.
+    Effects of mesoporous silica nanoparticles upon the function of mammalian sperm in vitro.
+    Nanomedicine. 2014 May;10(4):859-70.
+    </li>
+    <li>
+    Endesfelder U, Finan K, Holden SJ, Cook PR, Kapanidis AN, Heilemann M.
+    Multiscale spatial organization of RNA polymerase in Escherichia coli.
+    Biophys J. 2013 Jul 2;105(1):172-81. 
+    </li>
+    <li>
+    Villar G, Graham AD, Bayley H.
+    A tissue-like printed material.
+    Science. 2013 Apr 5;340(6128):48-52. 
+    </li>
+    <li>
+    Nowotschin S, Costello I, Piliszek A, Kwon GS, Mao CA, Klein WH, Robertson EJ, Hadjantonakis AK.
+    The T-box transcription factor Eomesodermin is essential for AVE induction in the mouse embryo.
+    Genes Dev. 2013 May 1;27(9):997-1002. 
+    </li>
+    
+    
+    
+    
+    </ul>
 	<h4>2012</h4>
     <ul class="publicationlist">
+    <li>
+    Davies TJ, Fairchild PJ.
+    Optimization of protocols for derivation of mouse embryonic stem cell lines from refractory strains, including the non obese diabetic mouse.
+    Stem Cells Dev. 2012 Jul 1;21(10):1688-700. 
+    </li>
     <li>Tavares L, Dimitrova E, Oxley D, Webster J, Poot R, Demmers J, Bezstarosti K, Taylor S, Ura H, Koide H, Wutz A, Vidal M, Elderkin S, Brockdorff N. (2012) RYBP-PRC1 Complexes Mediate H2A Ubiquitylation at Polycomb Target Sites Independently of PRC2 and H3K27me3. Cell. 2012 Feb 17;148(4):664-78.</li>
+    <li>
+    Wegmann F, Gartlan KH, Harandi AM, Brinckmann SA, Coccia M, Hillson WR, Kok WL, Cole S, Ho LP, Lambe T, Puthia M, Svanborg C, Scherer EM, Krashias G, Williams A, Blattman JN, Greenberg PD, Flavell RA, Moghaddam AE, Sheppard NC, Sattentau QJ.
+    Polyethyleneimine is a potent mucosal adjuvant for viral glycoprotein antigens.
+    Nat Biotechnol. 2012 Sep;30(9):883-8.
+    </li>
+    <li>
+    Roque H, Wainman A, Richens J, Kozyrska K, Franz A, Raff JW.
+    Drosophila Cep135/Bld10 maintains proper centriole structure but is dispensable for cartwheel formation.
+    J Cell Sci. 2012 Dec 1;125(Pt 23):5881-6. 
+    </li>
+    <li>Reschen RF, Colombie N, Wheatley L, Dobbelaere J, St Johnston D, Ohkura H, Raff JW.
+    Dgp71WD is required for the assembly of the acentrosomal Meiosis I spindle, and is not a general targeting factor for the γ-TuRC.
+    Biol Open. 2012 May 15;1(5):422-9. 
+    </li>
     </ul>
     <h4>2011</h4>
     <ul class="publicationlist">
